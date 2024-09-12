47b1f992a2c5dc5a341966502fba7d6b0743266c
chingis <chingis.bagdatov@gmail.com>
2024-09-12 20:23:18 +0500

    Solution for homework1

---
+++ Homework1/IOSHomework1Playground.playground/Sources/homework.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+let firstName = "Chingis"
+let lastName = "Bogdatov"
+var age = 20
+let birthYear = 2004
+var isStudent = true
+var height = 1.80
+
+let currentYear = 2024
+age = currentYear - birthYear
+
+
+let hobby = "Intellectual games"
+let numberOfHobbies = 4
+let favouriteNumber = 7
+let isHobbyCreative = false
+
+
+let hobbyDescription = if(isHobbyCreative){
+    "not creative"
+} else{
+    "creative"
+}
+
+
+let currentPosition = "Android Developer"
+let dreamCompany = "Reddit"
+let interestingTechnologies = "IOS, Backend"
+
+let lifeStory = """
+    My name is \(firstName) \(lastName). I am \(currentYear - birthYear) years old, born in \(birthYear)
+    I am currently a student. I enjoy \(hobby), which is a \(hobbyDescription).
+    I have \(numberOfHobbies) in total, and my favourite number is \(favouriteNumber).
+"""
+
+
+let lifeGoal = "I'm an \(currentPosition). I'm interested in \(interestingTechnologies). I want to work at \(dreamCompany)"
+
+print(lifeStory + "\n" + lifeGoal)
